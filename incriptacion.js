eacccfac5bf083cdc46ddd8fc05c5604dc76ce90
Tiberio <carvajal971715@gmail.com>
2023-10-21 11:37:22 -0500

    diccionario de datos

--- incriptacion.js
+++ incriptacion.js
@@ -1,7 +1,37 @@
-function incriptacion(){
+function incriptacion(mensaje){
+const letras= mensaje.split("")
+
+letras.map((Element))
+
+if (elemento=="a") return "1"
+if (elemento=="b") return "2"
+if (elemento=="c") return "3"
+if (elemento=="d") return "4"
+if (elemnto=="e") return "5"
+if (elemnto=="f") return "6"
+if (elemnto=="g") return "7"
+if (elemnto=="h") return "8"
+if (elemnto=="i") return "9"
+if (elemnto=="j") return "10"
+if (elemnto=="k") return "11"
+if (elemnto=="l") return "12"
+if (elemnto=="m") return "13"
+if (elemnto=="n") return "14"
+if (elemnto=="ñ") return "15"
+if (elemnto=="o") return "16"
+if (elemnto=="p") return "17"
+if (elemnto=="q") return "18"
+if (elemnto=="r") return "19"
+if (elemnto=="s") return "20"
+if (elemnto=="t") return "21"
+if (elemnto=="u") return "22"
+if (elemnto=="v") return "23" 
+if (elemnto=="w") return "24"
+if (elemnto=="x") return "25"
+if (elemnto=="y") return "26"
+if (elemnto=="z") return "27"
+if (elemnto==" ") return "28"
 
 
 
-
-    
 }
